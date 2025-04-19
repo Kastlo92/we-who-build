a2255c1b8d66e1dae4496536b625d0c7228bb2f3
Kastlo92 <castellanimichele92@gmail.com>
2025-04-19 13:08:18 +0200

    citium-laptop-v

---
+++ Scripts/GameDebugging.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var mainCamera = $MainCamera
+	var grid = $Grid
+	
+	#mainCamera.width = grid.width 
+	#mainCamera.position.y = grid.position.y + (grid.position.y)
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ Scripts/GridManager.gd
@@ -0,0 +1,27 @@
+extends Node
+
+const COLUMNS = 16;
+const ROWS = 16;
+var tileMapLayer;
+
+func _ready() -> void:	
+	tileMapLayer = get_node("TileMapLayer")
+	
+	pass 
+
+func _process(delta: float) -> void:		
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var fuff = tileMapLayer.get_local_mouse_position()			
+			var cell_coords = tileMapLayer.local_to_map(fuff)
+			insertTile(cell_coords)
+		
+func insertTile(position: Vector2i) -> void:
+	var tileData = tileMapLayer.get_cell_tile_data(position)
+	print(tileData.get_custom_data())
+	tileMapLayer.set_cell(position, 0, Vector2i(1,1))	
+
+	pass
