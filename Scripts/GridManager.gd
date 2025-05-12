87db06b0670adfb8b787d2b88d3d5e004e6fc24b
Kastlo92 <castellanimichele92@gmail.com>
2025-05-12 21:06:00 +0200

    drag to tile

--- Scripts/GridManager.gd
+++ Scripts/GridManager.gd
@@ -3,6 +3,9 @@ extends Node
 const COLUMNS = 16;
 const ROWS = 16;
 var tileMapLayer;
+var dragging = false;
+var click_radius = 32 # Size of the sprite.
+
 
 func _ready() -> void:	
 	tileMapLayer = get_node("TileMapLayer")
@@ -13,15 +16,30 @@ func _process(delta: float) -> void:
 	pass
 
 func _input(event):
+	#Posizionamento singola Tile
 	if event is InputEventMouseButton:
+		var localMousePos = tileMapLayer.get_local_mouse_position()			
+		var cell_coords = tileMapLayer.local_to_map(localMousePos)
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			var fuff = tileMapLayer.get_local_mouse_position()			
-			var cell_coords = tileMapLayer.local_to_map(fuff)
 			insertTile(cell_coords)
+			dragging = true
+		
+		#TRASCINAMENTO
+	if event is InputEventMouseMotion and dragging:
+		var localMousePos = tileMapLayer.get_local_mouse_position()			
+		var cell_coords = tileMapLayer.local_to_map(localMousePos)
+		print("dragging")
+		insertTile(cell_coords)
+
+	if not event.is_pressed():
+		dragging = false
 		
 func insertTile(position: Vector2i) -> void:
-	var tileData = tileMapLayer.get_cell_tile_data(position)
-	print(tileData)
-	tileMapLayer.set_cell(position, 1, Vector2i(1,1))	
-
+	#Attualmente il source ID è impostato su -1 perché stiamo scrivendo su tile "vuote".
+	#Se metto un terreno base sarà il caso di cambiare questa parte di codice 
+	if(tileMapLayer.get_cell_source_id(position) != -1):
+		print("position dragged")
+		pass
+	tileMapLayer.set_cell(position, 1, Vector2i(0,0))	
+	
 	pass
