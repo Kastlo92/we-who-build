f821888b5e27af39b87b7c679a966061a8080e20
Kastlo92 <castellanimichele92@gmail.com>
2025-07-14 23:33:34 +0200

    most recent changes : starting the game from menu

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,1 +1,12 @@
 extends Node
+
+var game_scene = preload("res://Scenes/game.tscn").instantiate()
+
+func _ready() -> void:
+	pass
+
+func _on_start_pressed() -> void:
+	get_tree().root.hide()
+	get_tree().root.add_child(game_scene)
+	
+	pass # Replace with function body.
